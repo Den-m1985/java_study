d2d2e7aee24e416c74be9b355a5d4c8052088f0f
den-m1985 <denismartyanov244@gmail.com>
2023-01-04 11:46:38 +0200

    добавил

---
+++ HttpServer.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+/* 
+https://habr.com/ru/post/441150/
+После запуска этого кода идем в окно браузера и набираем в адресной строке
+http://localhost:8080/
+Если все прошло удачно, то в окне браузера мы увидим текст «Привет всем»
+*/
+public class HttpServer {
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(8080)) {
+            System.out.println("Server started!");
+            
+            while (true) {
+                // ожидаем подключения
+                Socket socket = serverSocket.accept();
+                System.out.println("Client connected!");
+
+                // для подключившегося клиента открываем потоки 
+                // чтения и записи
+                try (BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+                     PrintWriter output = new PrintWriter(socket.getOutputStream())) {
+
+                    // ждем первой строки запроса
+                    while (!input.ready()) ;
+
+                    // считываем и печатаем все что было отправлено клиентом
+                    System.out.println();
+                    while (input.ready()) {
+                        System.out.println(input.readLine());
+                    }
+
+                    // отправляем ответ
+                    output.println("HTTP/1.1 200 OK");
+                    output.println("Content-Type: text/html; charset=utf-8");
+                    output.println();
+                    output.println("<p>Привет всем!</p>");
+                    output.flush();
+                    
+                    // по окончанию выполнения блока try-with-resources потоки, 
+                    // а вместе с ними и соединение будут закрыты
+                    System.out.println("Client disconnected!");
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
