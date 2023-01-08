b81872ff365ab755d0ad5ece38b0379e17100cb1
den-m1985 <denismartyanov244@gmail.com>
2023-01-08 17:58:02 +0200

    2

---
+++ javaJunior/If.java
@@ -0,0 +1,10 @@
+public class If {
+    public static void main(String[] args) {
+        int meInt = 5;
+        if (meInt < 10){
+            System.out.println("Да, верно");
+        } else if (meInt < 20) {
+            System.out.println("нет, не верно");
+        }
+    }
+}
