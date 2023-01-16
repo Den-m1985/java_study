21a01c7edff29ce906d315980b35b1443845afb2
den-m1985 <denismartyanov244@gmail.com>
2023-01-16 14:38:49 +0200

    add lesson 22

---
+++ javaJunior/Lesson22_final.java
@@ -0,0 +1,9 @@
+public class Lesson22_final {
+    public static void main(String[] args) {
+
+    }
+}
+class Test{
+    public static final int CONSTANT = 0; //final не изменяемое значение. Static значит для всего класса.
+
+}
