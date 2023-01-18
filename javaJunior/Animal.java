a8c38b6308c2041c5db85c583b2300ecbd957a7c
den-m1985 <denismartyanov244@gmail.com>
2023-01-18 14:42:41 +0200

    inheritance

---
+++ javaJunior/Animal.java
@@ -0,0 +1,10 @@
+public class Animal {
+    String name = "Some name";
+    public void eat(){
+        System.out.println("Animal is eating");
+    }
+    public void sleep(){
+        System.out.println("Animal is sleeping");
+    }
+
+}
