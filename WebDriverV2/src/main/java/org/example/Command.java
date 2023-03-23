ef958517608b0644f6b3da4750b87f5d5c54c14e
den-m1985 <denismartyanov244@gmail.com>
2023-03-23 15:57:19 +0200

    add to do

--- WebDriverV2/src/main/java/org/example/Command.java
+++ WebDriverV2/src/main/java/org/example/Command.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -21,44 +20,41 @@ public class Command {
 
         long start = System.nanoTime();
 
-        // Открываем браузер
+        // Open browser
         OpenChromeBrowser openBrowser = new OpenChromeBrowser();
         openBrowser.openChrome();
         WebDriver driver = openBrowser.getDriver();
         WebDriverWait wait = openBrowser.getWait();
 
+        // Open website
         new OpenWebSite(driver);
 
         // авторизуемся
         new LoginPage(wait);
 
-
-        String articul = "ЧЕХОЛ НА ТАБУРЕТ НА РЕЗИНКЕ МЯГКИЙ";
         // читаем csv
         int cellName = 0;   // Cell with name or articular
         int cellItem = 3;   // Cell with item to order
         CsvRead csvRead = new CsvRead(pathCSV);
         Map<String, String> data = csvRead.readCSV(cellName, cellItem);
 
-        Map<String, String> data2 = new HashMap<>();
-        data2.put("BESTWAY Круг для плавания, 56см, ПВХ, дизайнерский", "5");
+        //Map<String, String> data2 = new HashMap<>();
+        //data2.put("BESTWAY Круг для плавания, 56см, ПВХ, дизайнерский", "5");
 
-        for (Map.Entry<String, String> goods: data2.entrySet()) {
-            //goods.getKey();
-            //goods.getValue();
+        for (Map.Entry<String, String> goods: data.entrySet()) {
 
             SearchGoods searchGoods = new SearchGoods(wait);
             searchGoods.searchProduct(goods.getKey());
-
+            /*
+            добавить проверку на отсутствие товара
+             */
             AddGoods addGoods = new AddGoods(wait);
             addGoods.addGoods(goods.getValue());
 
         }
 
-
         List<String> list = new ArrayList<>();
         //new WrightOldExelArticul(list);
-
 
         long end = System.nanoTime();
         long a = end - start;
@@ -70,7 +66,7 @@ public class Command {
         System.out.println();
         System.out.println("Попей чайку");
         System.out.println();
-
     }
+
 
 }
